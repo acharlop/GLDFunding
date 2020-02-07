77907226efb4b5a4664eab5aab5227456d052985
Maite Rosalie <maite.rosalie@gmail.com>
2020-02-07 12:59:53 -0300

    [style] Fix CTA layout on mobile

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -32,16 +32,21 @@ export const CTA = () => (
   <Wrapper as="section">
     <Background src="./static/images/footer-image.jpg" alt="mountains background" />
     <Flex flexWrap="wrap" m="auto" maxWidth="1200px" py={[5, 8]} alignItems="center">
-      <Box width={[1, 1, 8 / 12, 9 / 12]} px={2}>
-        <Text as="h1" fontSize={[4, 5, '44px']} mb={[1, 2]} color="white" fontFamily="heading">
+      <Text
+        width={[1, 8 / 12, 9 / 12]}
+        px={2}
+        fontFamily="heading"
+        textAlign={['center', 'left', 'left']}
+      >
+        <Text as="h1" fontSize={[4, 5, '44px']} mb={[1, 2]} color="white">
           An unforgettable experience
         </Text>
-        <Text as="p" fontSize={[3, 4, '44px']} color="green500" fontFamily="heading">
+        <Text as="p" fontSize={[3, 4, '44px']} color="green500">
           Join the community.
         </Text>
-      </Box>
-      <Box width={[1, 3 / 4, 4 / 12, 3 / 12]} p={2}>
-        <Button width={1} my={1} minHeight="45px" rounded>
+      </Text>
+      <Box width={[1, 4 / 12, 3 / 12]} p={2}>
+        <Button width={1} my={1} minHeight="45px" maxWidth="300px" m="auto" rounded>
           CONTACT US
         </Button>
         <Text
@@ -51,6 +56,8 @@ export const CTA = () => (
           textAlign="center"
           lineHeight="heading"
           mt={3}
+          maxWidth="400px"
+          mx="auto"
         >
           Do you have a question? <br />
           Contact Us.
