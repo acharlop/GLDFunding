5b6868e2f7cbd107a5e4d9a6bba5f9f5d7e02ed4
MaiteRosalie <maite.rosalie@gmail.com>
2020-04-17 08:38:42 -0300

    sentry setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import App from 'next/app';
 import { NextSeo } from 'next-seo';
 import { ThemeProvider } from 'emotion-theming';
+import * as Sentry from '@sentry/browser';
 
 import theme from '../theme';
 import { GlobalStyles } from '../theme/globalStyles';
 import { OrganizationLd, SEO } from '../seo';
+
+Sentry.init({ dsn: 'https://aea1ec5abc6b4031b494b8dfbafa97dc@o379266.ingest.sentry.io/5203865' });
 
 class AppComponent extends App {
   render() {
